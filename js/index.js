fa05cff2021cedfefd718eeb31e977bec55c67cd
alanseraf <alan.seraf@gmail.com>
2021-12-20 07:55:03 -0300

    JS: Mundando a cor de fundo do código

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
+(()=> {
 var input = document.querySelector('[data-form-input]');
 
 input.onchange = trocaCor;
 
 function trocaCor (evento){
-    console.log(input)
-
+    evento.preventDefault() /*Impedindo de recarregar a página*/
+    corValor = input.value
+    console.log(corValor)
+    var fundo = document.querySelector('[data-form-fundo]');
+    fundo.style.backgroundColor = input.value
 }
+})()
